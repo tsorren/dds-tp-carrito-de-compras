66c8fba52b32882db54ed315934e3e10ddb13f82
BerEsti <mestigarribia@frba.utn.edu.ar>
2026-04-23 18:23:02 -0300

    test productos

--- tests/dominio/carrito/ProductoTest.java
+++ tests/dominio/carrito/ProductoTest.java
@@ -15,13 +15,47 @@ class ProductoTest {
     @BeforeEach
     void setup() {
         p = new Producto();
-
-        // Definir historial de precios
     }
 
     @Test
     void getPrecio() {
         // Hacer que el precio actual del producto sea 100.0
+        PrecioProducto precio = new PrecioProducto();
+        precio.setPrecio(100.0);
+        precio.setFehcaInicioVigencia(fecha.minusDays(10));
+        precio.setFechaFinVigencia(fecha.plusDays(10));
+        p.agregarNuevoPrecio(precio);
+
+        assertEquals(100.0, p.getPrecio(fecha));
+    }
+
+    @Test
+    void getPrecioFueraDeVigencia() {
+        PrecioProducto precio = new PrecioProducto();
+        precio.setPrecio(100.0);
+        precio.setFehcaInicioVigencia(fecha.minusDays(10));
+        precio.setFechaFinVigencia(fecha.minusDays(1)); // venció antes de la fecha
+        p.agregarNuevoPrecio(precio);
+
+        assertNull(p.getPrecio(fecha));
+    }
+
+    @Test
+    void getPrecioConHistorial() {
+        // precio viejo ya vencido
+        PrecioProducto precioViejo = new PrecioProducto();
+        precioViejo.setPrecio(50.0);
+        precioViejo.setFehcaInicioVigencia(fecha.minusDays(20));
+        precioViejo.setFechaFinVigencia(fecha.minusDays(1));
+        p.agregarNuevoPrecio(precioViejo);
+
+        // precio vigente en la fecha
+        PrecioProducto precioNuevo = new PrecioProducto();
+        precioNuevo.setPrecio(100.0);
+        precioNuevo.setFehcaInicioVigencia(fecha);
+        precioNuevo.setFechaFinVigencia(fecha.plusDays(10));
+        p.agregarNuevoPrecio(precioNuevo);
+
         assertEquals(100.0, p.getPrecio(fecha));
     }
 }
