f8079ea19c63511b16c7e75864c4505076611d5d
Tadeo <tsorrentino@frba.utn.edu.ar>
2026-04-23 22:41:28 -0300

    Tests completados

--- tests/dominio/cliente/ClienteTest.java
+++ tests/dominio/cliente/ClienteTest.java
@@ -1,17 +1,47 @@
 package dominio.cliente;
 
+import dominio.carrito.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class ClienteTest {
 
     Cliente c;
+    Carrito ca;
+
+    Item i;
+    Producto p;
+    Tarjeta t;
 
     @BeforeEach
     void setup() {
-        Cliente.setLimiteDeuda(500.0);
+        c = new Cliente();
+
+        ca = new Carrito();
+        i = new Item();
+        p = new Producto();
+
+        t = new Tarjeta();
+        t.setNombre(c.getNombre());
+        t.setUltimosDigitos("2839");
+
+        PrecioProducto precioInicial = new PrecioProducto();
+        precioInicial.setPrecio(100.0);
+        precioInicial.setFechaInicioVigencia(LocalDate.now().minusDays(10));
+        precioInicial.setFechaFinVigencia(LocalDate.now().plusDays(10));
+        p.agregarNuevoPrecio(precioInicial);
+
+        i.setProducto(p);
+        i.setCantidad(5);
+        i.setPrecioUnitario(p.getPrecio(LocalDate.now()));
+
+        ca.agregarItem(i);
+
+        c.agregarCarrito(ca);
 
         // Settear cliente con carritos y productos
     }
@@ -20,6 +50,12 @@ class ClienteTest {
     void getMontoDeuda() {
         // Hacer que el monto deuda sea 300.0
 
+        Pago pago = new Pago();
+        pago.setMonto(200.0);
+        pago.setCarrito(ca);
+        pago.setTarjeta(t);
+
+        ca.agregarPago(pago);
         assertEquals(300.0, c.getMontoDeuda());
     }
 
@@ -27,6 +63,7 @@ class ClienteTest {
     void estaHabilitado() {
         // Hacer que el monto deuda sea menor al limite deuda
 
+        Cliente.setLimiteDeuda(1000.0);
         assertTrue(c.estaHabilitado());
     }
 
@@ -34,6 +71,7 @@ class ClienteTest {
     void noEstaHabilitado() {
         // Hacer que el monto deuda sea mayor al limite deuda
 
+        Cliente.setLimiteDeuda(300.0);
         assertFalse(c.estaHabilitado());
     }
 }
